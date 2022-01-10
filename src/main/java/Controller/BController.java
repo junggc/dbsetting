627aba24db8466cdc1b30d8b88ffe0252b667863
jung.kacelab <jung@kacelab.com>
2022-01-10 22:12:44 +0900

    Initial commit

---
+++ src/main/java/Controller/BController.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BController {
+
+    @RequestMapping("/")
+    public String main(Model model){
+        return "index";
+
+}
+
+}
---
+++ src/test/java/Controller/DBConnectiontest.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+
+@ContextConfiguration(locations = {"file:web/WEB-INF/*Context.xml"})
+
+public class DBConnectiontest {
+    @Autowired
+    private DataSource ds;
+
+    @Test
+    public void testConnection(){
+        try(Connection con = ds.getConnection()){
+            System.out.println(("MariaDB 연결 성공 : " + con));
+        }catch (Exception e) { e.printStackTrace();
+        }
+    }
+}
+
+
